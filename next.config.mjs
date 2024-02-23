29beeeec7d5ba87cedaf5d3be7a8471cb974ccfc
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-02-23 18:47:39 +0700

    project setup & authentication with clerk

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadting.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'subdomain.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
+  },
+  reactStrictMode: false,
+}
 
 export default nextConfig
---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  return <div className="flex h-full items-center justify-center">{children}</div>
+}
+
+export default AuthLayout
